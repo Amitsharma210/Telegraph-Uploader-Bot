5784e7421b704130a855bb8eab56addbb56cd78b
Amitsharma210 <144205854+Amitsharma210@users.noreply.github.com>
2024-03-25 16:39:06 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -2,8 +2,8 @@ import os
 
 class Config(object):
     # Get a token from @BotFather
-    BOT_TOKEN = os.environ["BOT_TOKEN"]
+    BOT_TOKEN = os.environ["7027151572:AAGEA_QCrOf897TBQ0jAKP4q6GMFU8uuArg"]
     
     # Get these values from my.telegram.org
-    API_ID = int(os.environ["API_ID"])
-    API_HASH = os.environ["API_HASH"]
+    API_ID = int(os.environ["22164996"])
+    API_HASH = os.environ["035f2448fefaf28659b05af05142b45b"]
